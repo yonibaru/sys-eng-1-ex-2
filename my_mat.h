da19348a9d65e76659d390d923f892f04e769d7d
yonibaru <theyonib@gmail.com>
2024-02-12 11:21:48 +0200

    maybe now will this test work :(?

--- my_mat.h
+++ my_mat.h
@@ -8,8 +8,8 @@
 //my_mat.c
 void floydWarshal(int matrix[10][10],int dist[10][10]);
 void enterMatrix(int matrix[10][10]);
-int shortestPath(int matrix[10][10]);
-int pathExists(int matrix[10][10]);
+void shortestPath(int matrix[10][10]);
+void pathExists(int matrix[10][10]);
 
 //my_graph.c
 
