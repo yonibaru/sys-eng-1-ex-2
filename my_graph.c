17e0e54b490f59e097ca5b67a66da656f5b77995
yonibaru <theyonib@gmail.com>
2024-02-12 12:51:40 +0200

    maybe now please work

--- my_graph.c
+++ my_graph.c
@@ -22,39 +22,36 @@ int main(){
         switch (currentFunction){
             case 'A':
             case 'a':
-                // enterMatrix(mainMatrix);
-                // floydWarshal(mainMatrix,dist);
+                enterMatrix(mainMatrix);
+                floydWarshal(mainMatrix,dist);
 
-                 int testMatrix[SIZE][SIZE] = {
-                    {0, 37, 19, 24, 26, 11, 29, 41, 22, 41},
-                    {37, 0, 28, 25, 31, 38, 14, 6, 0, 1},
-                    {19, 28, 0, 36, 42, 12, 39, 22, 39, 22},
-                    {24, 25, 36, 0, 16, 23, 26, 5, 12, 38},
-                    {26, 31, 42, 16, 0, 31, 23, 39, 31, 9},
-                    {11, 38, 12, 23, 31, 0, 48, 30, 5, 1},
-                    {29, 14, 39, 26, 23, 48, 0, 46, 6, 31},
-                    {41, 6, 22, 5, 39, 30, 46, 0, 29, 40},
-                    {22, 0, 39, 12, 31, 5, 6, 29, 0, 11},
-                    {41, 1, 22, 38, 9, 1, 31, 40, 11, 0}
-                    };
-                floydWarshal(testMatrix,dist);
+                //  int testMatrix[SIZE][SIZE] = {
+                //     {0, 37, 19, 24, 26, 11, 29, 41, 22, 41},
+                //     {37, 0, 28, 25, 31, 38, 14, 6, 0, 1},
+                //     {19, 28, 0, 36, 42, 12, 39, 22, 39, 22},
+                //     {24, 25, 36, 0, 16, 23, 26, 5, 12, 38},
+                //     {26, 31, 42, 16, 0, 31, 23, 39, 31, 9},
+                //     {11, 38, 12, 23, 31, 0, 48, 30, 5, 1},
+                //     {29, 14, 39, 26, 23, 48, 0, 46, 6, 31},
+                //     {41, 6, 22, 5, 39, 30, 46, 0, 29, 40},
+                //     {22, 0, 39, 12, 31, 5, 6, 29, 0, 11},
+                //     {41, 1, 22, 38, 9, 1, 31, 40, 11, 0}
+                //     };
+                // floydWarshal(testMatrix,dist);
                  
                 break;
             case 'B':
             case 'b':
-                printf("\n");
                 pathExists(dist);
                 break;
             case 'C':
             case 'c':
-                printf("\n");
                 shortestPath(dist);
                 break;
             case 'D':
             case 'd':
                 return 0;
             default:
-                printf("Invalid input. Please enter a char A-D.\n");
                 break;
         }
     }
